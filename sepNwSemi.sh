0e3cb39b72c715f0a779a57b0eeb287fde666afa
Theo <gmbaxevanos@gmx.ch>
2019-06-06 17:22:33 +0200

    sepNSemi

---
+++ sepNwSemi.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#In this challenge, we practice using the paste command to merge lines of a given file.You are given a CSV file where each row contains the name of a city and its state separated by a comma. Your task is to replace the newlines in the file with semicolons
+
+paste -sd';'
+
