cc9d791c3b9dc2c4bd4c5c0a6ba3b1497b3e8b6b
Theo <gmbaxevanos@gmx.ch>
2019-10-15 00:04:48 +0200

    rab

---
+++ replaceAddBrack.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#Given an input file, in each line, highlight all the occurrences of 'thy' by wrapping them up in brace brackets. The search should be case-insensitive.
+
+sed -e 's/[tT]hy/{&}/g'
